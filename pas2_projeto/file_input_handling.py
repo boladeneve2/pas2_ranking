3404065ced5958d02776f5084e040018d62d8196
boladeneve2 <108742034+boladeneve2@users.noreply.github.com>
2025-01-29 21:49:13 -0300

    Delete pas2_projeto directory

--- pas2_projeto/file_input_handling.py
+++
@@ -1,11 +0,0 @@
-
-
-
-
-
-with open('.idea/text_files/pas2_resultado_teste.txt', 'r') as infile, open('.idea/text_files/pas2_resultado_limpo.txt', 'w') as outfile:
-    temp = infile.read().replace("", "")
-    temp = temp.replace("\n", "")
-    temp = temp.replace("/", "\n")
-    outfile.write(temp)
-
--- pas2_projeto/teste.py
+++
@@ -1,5 +0,0 @@
-
-
-
-x = isinstance(input(), int)
-print(x)
